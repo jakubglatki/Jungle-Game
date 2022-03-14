71e94e2df1b4d4f909e800fc50c1393ae3c62bf9
claudiosavelli <claudiosav2000@gmail.com>
2022-03-14 17:36:45 +0000

    1

--- Application.py
+++ Application.py
@@ -23,8 +23,8 @@ e2 = Animal("elephant",8,2,2,6)
 Animals1 = [m1, c1, d1, w1, p1, t1, l1, e1]
 Animals2 = [m2, c2, d2, w2, p2, t2, l2, e2]
 
-player1 = Player(1,Animals1)
-player2 = Player(2,Animals2)
+player1 = Player(1,Animals1,"human")
+player2 = Player(2,Animals2,"human")
 board = Board(Animals1, Animals2)
 actual = player1
 
@@ -36,7 +36,33 @@ def noPossibleMoveForPlayer(player: Player):
 
 while(testFinalGame==True):
     print("Turn of player"+actual.number)
-    print("")
+    if(actual.isABot==False):
+        #first while for chosing a valid starting point
+        while():
+            print("select starting point of your move: ")
+            sp = str(input())
+            x1=ord(sp[0].lower())-97
+            y1=sp[1]
+            if (isValid(x1,y1)):
+                exit
+            else: print("The starting point selected is not fine.")
+        #second while for chosing a valid ending point
+        while():
+            print("select ending point of your move: ")
+            ep = str(input())
+            x2=ord(ep[0].lower())-97
+            y2=ep[1]
+            if (isValid2(ep)):
+                exit
+            else: print("The ending point selected is not fine.")
+
+    #The movement is valid, now we have just to make it and verify if, at the end of the movement, the player have eaten something off the other one
+
+
+
+
+
+
 
     if(actual==player1):
         actual=player2
--- Game/Player.py
+++ Game/Player.py
@@ -1,7 +1,10 @@
 class Player:
-    def __init__(self, number, AnimalCollection):
+    def __init__(self, number: int, AnimalCollection: list, robot: str):
         self.number = number
         self.alive = 8
+        if (robot=="IA"):
+            self.isARobot=True
+        else: self.isABot=False
 
 
 
