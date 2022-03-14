6f2661bfc28ad7da9e707733c5d1b7b6efb88f45
jakubglatki <32642333+jakubglatki@users.noreply.github.com>
2022-03-14 19:09:33 +0000

    +Added class for displaying board to player +added field boardField to Player class #repaired position of rat of player2

--- model/Animal.py
+++ model/Animal.py
@@ -1,14 +1,11 @@
 from Game.Player import Player
 
-
 class Animal:
-    def __init__(self,name: str,power: int,player: Player, x: int, y: int):
+    def __init__(self,name: str, fieldName: str, power: int,player: Player, x: int, y: int):
         self.name = name
+        self.fieldName = fieldName
         self.power = power
         self.player = player
         self.x = x
         self.y = y
         self.isAlive = True
-
-
-
---
+++ view/BoardViewer.py
@@ -0,0 +1,26 @@
+from model.Board import Board
+
+
+class BoardViewer:
+    ROWS = 9
+    COLUMNS = 7
+
+    def __init__(self, board: Board):
+        self.board = board
+
+    def showBoard(self):
+        boardView = " A   B   C   D   E   F   G \n"
+        for index1, row in enumerate(reversed(self.board.matrix)):
+            for index2, cell in enumerate(row):
+                boardView += self.putAnimalOnBoard(index1, index2, cell)
+            boardView += " " + str(index1 + 1)
+            boardView += "\n"
+        print(boardView)
+        return boardView
+
+    def putAnimalOnBoard(self, x: int, y: int, cell):
+        if self.board.matrix[x][y].Animal is not None:
+            return " " + self.board.matrix[x][y].Animal.fieldName + " "
+        else:
+            return " XX " if cell.kind == 1 else " ~~ " if cell.kind == 2 else " ## " \
+                if cell.kind == 3 else " 00 " if cell.kind == 4 else " ** "
