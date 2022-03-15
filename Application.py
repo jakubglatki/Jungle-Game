b438b8a96c8a9e250fcf82356dddc0ea90e77f81
claudiosavelli <claudiosav2000@gmail.com>
2022-03-15 21:41:39 +0000

    1

--- Application.py
+++ Application.py
@@ -35,6 +35,7 @@ actual = player1
 boardViewer = BoardViewer(board)
 gameController = GameController(boardViewer)
 gameController.chooseGameMode()
+humanRoundController = HumanRoundController()
 
 
 def testFinalGame():
@@ -48,34 +49,7 @@ def noPossibleMoveForPlayer(player: Player):
 while (testFinalGame == True):
     print("Turn of player" + actual.number)
     if (actual.isABot == False):
-        # first while for chosing a valid starting point
-        while ():
-            print("select starting point of your move: ")
-            sp = str(input())
-            x1 = ord(sp[0].lower()) - 97
-            y1 = sp[1]
-            if (isValid(x1,
-                        y1)):  # In this test we see if in the selected area there is one of your animal and if he can move
-                exit
-            else:
-                print("The starting point selected is not fine.")
-        # second while for chosing a valid ending point
-        while ():
-            print("select ending point of your move: ")
-            ep = str(input())
-            x2 = ord(ep[0].lower()) - 97
-            y2 = ep[1]
-            if (isValid2(
-                    ep)):  # In this test we see if: the animal can go there, if there is another weaker opponent animal (in case of one of yours or in case of stronger you can't), if there is some special rule applied
-                exit
-            else:
-                print("The ending point selected is not fine.")
-
-    # The movement is valid, now we have just to make it and verify if, at the end of the movement, the player have eaten something off the other one
-    # In case in the ending point there is another animal, kill it
-    board.killAnimal(x2, y2)
-    board.moveAnimal()  # to do, simply move the animal from (x1,y1) to (x2,y2)
-
+        humanRoundController.round(actual,board)
     if (actual == player1):
         actual = player2
     else:
