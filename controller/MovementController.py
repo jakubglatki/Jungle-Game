1759613c636ba2c8db285a8bd37b07e4f09de542
jakubglatki <32642333+jakubglatki@users.noreply.github.com>
2022-03-17 20:18:28 +0000

    #organized code in Board and Cell classes

--- controller/MovementController.py
+++ controller/MovementController.py
@@ -1,18 +1,18 @@
-from model.Animal import Animal
+from model.animal import Animal
 from model.Board import Board
 
 
 class MovementController:
 
     def killAnimal(self, board: Board, x: int, y: int):
-        if board.matrix[x][y].Animal != None:
-            board.matrix[x][y].Animal.player.alive = board.matrix[x][y].Animal.player.alive - 1
-            board.matrix[x][y].Animal.isAlive = False
-            board.matrix[x][y].Animal.x = -1
-            board.matrix[x][y].Animal.y = -1
+        if board.matrix[x][y].animal != None:
+            board.matrix[x][y].animal.player.alive = board.matrix[x][y].animal.player.alive - 1
+            board.matrix[x][y].animal.isAlive = False
+            board.matrix[x][y].animal.x = -1
+            board.matrix[x][y].animal.y = -1
 
     def moveAnimal(self, animal: Animal, board: Board, x: int, y: int):
-        board.matrix[int(animal.x)][int(animal.y)].Animal = None
-        board.matrix[x][y].Animal = animal
+        board.matrix[int(animal.x)][int(animal.y)].animal = None
+        board.matrix[x][y].animal = animal
         animal.x = x
         animal.y = y
