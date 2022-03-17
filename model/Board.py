20c91dfe0257ec77e39041856484d96f0051c979
jakubglatki <32642333+jakubglatki@users.noreply.github.com>
2022-03-17 20:16:55 +0000

    #organized code in Board and Cell classes

--- model/Board.py
+++ model/Board.py
@@ -3,35 +3,35 @@ from model.Cell import Cell
 
 class Board:
     def __init__(self, Animals1: list, Animals2: list):
-        #Generate the board
+        # Generate the board
         self.matrix = [[Cell(1), Cell(1), Cell(3), Cell(5), Cell(3), Cell(1), Cell(1)],
-                  [Cell(1), Cell(1), Cell(1), Cell(3), Cell(1), Cell(1), Cell(1)],
-                  [Cell(1), Cell(1), Cell(1), Cell(1), Cell(1), Cell(1), Cell(1)],
-                  [Cell(1), Cell(2), Cell(2), Cell(1), Cell(2), Cell(2), Cell(1)],
-                  [Cell(1), Cell(2), Cell(2), Cell(1), Cell(2), Cell(2), Cell(1)],
-                  [Cell(1), Cell(2), Cell(2), Cell(1), Cell(2), Cell(2), Cell(1)],
-                  [Cell(1), Cell(1), Cell(1), Cell(1), Cell(1), Cell(1), Cell(1)],
-                  [Cell(1), Cell(1), Cell(1), Cell(3), Cell(1), Cell(1), Cell(1)],
-                  [Cell(1), Cell(1), Cell(3), Cell(4), Cell(3), Cell(1), Cell(1)]]
-        #put the animals in their starting points
-        self.matrix[2][0].Animal=Animals2[0]
-        self.matrix[1][5].Animal=Animals2[1]
-        self.matrix[1][1].Animal=Animals2[2]
-        self.matrix[2][4].Animal=Animals2[3]
-        self.matrix[2][2].Animal=Animals2[4]
-        self.matrix[0][6].Animal=Animals2[5]
-        self.matrix[0][0].Animal=Animals2[6]
-        self.matrix[2][6].Animal=Animals2[7]
+                       [Cell(1), Cell(1), Cell(1), Cell(3), Cell(1), Cell(1), Cell(1)],
+                       [Cell(1), Cell(1), Cell(1), Cell(1), Cell(1), Cell(1), Cell(1)],
+                       [Cell(1), Cell(2), Cell(2), Cell(1), Cell(2), Cell(2), Cell(1)],
+                       [Cell(1), Cell(2), Cell(2), Cell(1), Cell(2), Cell(2), Cell(1)],
+                       [Cell(1), Cell(2), Cell(2), Cell(1), Cell(2), Cell(2), Cell(1)],
+                       [Cell(1), Cell(1), Cell(1), Cell(1), Cell(1), Cell(1), Cell(1)],
+                       [Cell(1), Cell(1), Cell(1), Cell(3), Cell(1), Cell(1), Cell(1)],
+                       [Cell(1), Cell(1), Cell(3), Cell(4), Cell(3), Cell(1), Cell(1)]]
+        # put the animals in their starting points
+        self.matrix[2][0].animal = Animals2[0]
+        self.matrix[1][5].animal = Animals2[1]
+        self.matrix[1][1].animal = Animals2[2]
+        self.matrix[2][4].animal = Animals2[3]
+        self.matrix[2][2].animal = Animals2[4]
+        self.matrix[0][6].animal = Animals2[5]
+        self.matrix[0][0].animal = Animals2[6]
+        self.matrix[2][6].animal = Animals2[7]
 
-        self.matrix[6][6].Animal=Animals1[0]
-        self.matrix[7][1].Animal=Animals1[1]
-        self.matrix[7][5].Animal=Animals1[2]
-        self.matrix[6][2].Animal=Animals1[3]
-        self.matrix[6][4].Animal=Animals1[4]
-        self.matrix[8][0].Animal=Animals1[5]
-        self.matrix[8][6].Animal=Animals1[6]
-        self.matrix[6][0].Animal=Animals1[7]
+        self.matrix[6][6].animal = Animals1[0]
+        self.matrix[7][1].animal = Animals1[1]
+        self.matrix[7][5].animal = Animals1[2]
+        self.matrix[6][2].animal = Animals1[3]
+        self.matrix[6][4].animal = Animals1[4]
+        self.matrix[8][0].animal = Animals1[5]
+        self.matrix[8][6].animal = Animals1[6]
+        self.matrix[6][0].animal = Animals1[7]
 
-    def killAnimal(self,x1:int,y1:int):
-        if(self.matrix[x1][y1].thereIsAnimal()):
+    def killAnimal(self, x1: int, y1: int):
+        if (self.matrix[x1][y1].thereIsAnimal()):
             self.matrix[x1][y1].killAnimal()
--- model/Cell.py
+++ model/Cell.py
@@ -1,18 +1,27 @@
+from model.Animal import Animal
+
+
 class Cell:
-    def __init__(self,kind):
+    def __init__(self, kind):
         self.kind = kind
         # 1: grass - 2: water - 3: traps - 4: dojo1 - 5: dojo2
-    Animal = None
-    def addAnimal(self,Animal: Animal):
-        self.Animal = Animal
+
+    animal = None
+
+    def addAnimal(self, animal: Animal):
+        self.animal = animal
+
     def removeAnimal(self):
-        self.Animal = None
+        self.animal = None
+
     def thereIsAnimal(self):
-        if(self.Animal!=None): return True
-        else: return False
+        if (self.animal != None):
+            return True
+        else:
+            return False
 
     def killAnimal(self):
-        self.Animal.x=-1
-        self.Animal.y=-1
-        self.Animal.isAlive=False
-        self.Animal.player.alive=self.Animal.player.alive-1
+        self.animal.x = -1
+        self.animal.y = -1
+        self.animal.isAlive = False
+        self.animal.player.alive = self.animal.player.alive - 1
