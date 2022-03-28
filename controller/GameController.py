89bd3ac04bbab765d87991c3569d515a8697e126
Claudio Savelli <claudiosav2000@gmail.com>
2022-03-28 18:15:46 +0100

    First edition of simplified game

--- controller/GameController.py
+++ controller/GameController.py
@@ -125,7 +125,7 @@ class GameController:
         return
 
     def researchMode(self):
-        n = 90
+        n = 10
         actual = 0
         nwins1 = 0
         nwins2 = 0
--- model/Board.py
+++ model/Board.py
@@ -12,25 +12,25 @@ class Board:
             if isPlayer1Bot == True: self.player1.switchToBot()
             if isPlayer2Bot == True: self.player2.switchToBot()
 
-            m1 = Animal("MOUSE", "M1", 1, self.player1, 6, 6)
+            #m1 = Animal("MOUSE", "M1", 1, self.player1, 6, 6)
             c1 = Animal("CAT", "C1", 2, self.player1, 7, 1)
             d1 = Animal("DOG", "D1", 3, self.player1, 7, 5)
             w1 = Animal("WOLF", "W1", 4, self.player1, 6, 2)
             p1 = Animal("PANTHER", "P1", 5, self.player1, 6, 4)
-            t1 = Animal("TIGER", "T1", 6, self.player1, 8, 0)
-            l1 = Animal("LION", "L1", 7, self.player1, 8, 6)
-            e1 = Animal("ELEPHANT", "E1", 8, self.player1, 6, 0)
-            m2 = Animal("MOUSE", "M2", 1, self.player2, 2, 0)
+            #t1 = Animal("TIGER", "T1", 6, self.player1, 8, 0)
+            #l1 = Animal("LION", "L1", 7, self.player1, 8, 6)
+            #e1 = Animal("ELEPHANT", "E1", 8, self.player1, 6, 0)
+            #m2 = Animal("MOUSE", "M2", 1, self.player2, 2, 0)
             c2 = Animal("CAT", "C2", 2, self.player2, 1, 5)
             d2 = Animal("DOG", "D2", 3, self.player2, 1, 1)
             w2 = Animal("WOLF", "W2", 4, self.player2, 2, 4)
             p2 = Animal("PANTHER", "P2", 5, self.player2, 2, 2)
-            t2 = Animal("TIGER", "T2", 6, self.player2, 0, 6)
-            l2 = Animal("LION", "L2", 7, self.player2, 0, 0)
-            e2 = Animal("ELEPHANT", "E2", 8, self.player2, 2, 6)
+            #t2 = Animal("TIGER", "T2", 6, self.player2, 0, 6)
+            #l2 = Animal("LION", "L2", 7, self.player2, 0, 0)
+            #e2 = Animal("ELEPHANT", "E2", 8, self.player2, 2, 6)
 
-            self.Animals1 = [e1, l1, t1, p1, w1, d1, c1, m1]
-            self.Animals2 = [e2, l2, t2, p2, w2, d2, c2, m2]
+            self.Animals1 = [p1, w1, d1, c1]
+            self.Animals2 = [p2, w2, d2, c2]
 
             self.player1.animalCollection = self.Animals1
             self.player2.animalCollection = self.Animals2
@@ -45,23 +45,23 @@ class Board:
                            [Cell(1), Cell(1), Cell(1), Cell(3), Cell(1), Cell(1), Cell(1)],
                            [Cell(1), Cell(1), Cell(3), Cell(4), Cell(3), Cell(1), Cell(1)]]
             # put the animals in their starting points
-            self.matrix[2][0].animal = self.Animals2[7]
-            self.matrix[1][5].animal = self.Animals2[6]
-            self.matrix[1][1].animal = self.Animals2[5]
-            self.matrix[2][4].animal = self.Animals2[4]
-            self.matrix[2][2].animal = self.Animals2[3]
-            self.matrix[0][6].animal = self.Animals2[2]
-            self.matrix[0][0].animal = self.Animals2[1]
-            self.matrix[2][6].animal = self.Animals2[0]
+            #self.matrix[2][0].animal = self.Animals2[7]
+            self.matrix[1][5].animal = self.Animals2[3]
+            self.matrix[1][1].animal = self.Animals2[2]
+            self.matrix[2][4].animal = self.Animals2[1]
+            self.matrix[2][2].animal = self.Animals2[0]
+            #self.matrix[0][6].animal = self.Animals2[2]
+            #self.matrix[0][0].animal = self.Animals2[1]
+            #self.matrix[2][6].animal = self.Animals2[0]
 
-            self.matrix[6][6].animal = self.Animals1[7]
-            self.matrix[7][1].animal = self.Animals1[6]
-            self.matrix[7][5].animal = self.Animals1[5]
-            self.matrix[6][2].animal = self.Animals1[4]
-            self.matrix[6][4].animal = self.Animals1[3]
-            self.matrix[8][0].animal = self.Animals1[2]
-            self.matrix[8][6].animal = self.Animals1[1]
-            self.matrix[6][0].animal = self.Animals1[0]
+            #self.matrix[6][6].animal = self.Animals1[7]
+            self.matrix[7][1].animal = self.Animals1[3]
+            self.matrix[7][5].animal = self.Animals1[2]
+            self.matrix[6][2].animal = self.Animals1[1]
+            self.matrix[6][4].animal = self.Animals1[0]
+            #self.matrix[8][0].animal = self.Animals1[2]
+            #self.matrix[8][6].animal = self.Animals1[1]
+            #self.matrix[6][0].animal = self.Animals1[0]
 
 
     def getDojo1(self):
